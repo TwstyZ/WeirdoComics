32d8ba25bbb04804459274d388ff1b21f8718902
urielduana <81336230+urielduana@users.noreply.github.com>
2022-12-04 22:22:31 -0600

    A3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\comicController;
+use App\Http\Controllers\itemController;
+use App\Http\Controllers\orderController;
+use App\Http\Controllers\providerController;
+use App\Http\Controllers\saleController;
+use App\Http\Controllers\sellerController;
+
 
 
 /*
